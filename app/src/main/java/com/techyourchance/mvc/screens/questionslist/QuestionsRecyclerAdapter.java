7fd6980931dcf6c347fbf10484b1456660d7811e
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2018-07-30 12:28:27 +0300

    Refactoring QuestionsList screen to use RecyclerView

---
+++ app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsRecyclerAdapter.java
@@ -0,0 +1,70 @@
+package com.techyourchance.mvc.screens.questionslist;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.techyourchance.mvc.questions.Question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionsRecyclerAdapter extends RecyclerView.Adapter<QuestionsRecyclerAdapter.MyViewHolder>
+        implements QuestionsListItemViewMvc.Listener {
+
+    public interface Listener {
+        void onQuestionClicked(Question question);
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private final QuestionsListItemViewMvc mViewMvc;
+
+        public MyViewHolder(QuestionsListItemViewMvc viewMvc) {
+            super(viewMvc.getRootView());
+            mViewMvc = viewMvc;
+        }
+
+    }
+
+    private final LayoutInflater mInflater;
+    private final Listener mListener;
+
+    private List<Question> mQuestions = new ArrayList<>();
+
+    public QuestionsRecyclerAdapter(LayoutInflater inflater, Listener listener) {
+        mInflater = inflater;
+        mListener = listener;
+    }
+
+    public void bindQuestions(List<Question> questions) {
+        mQuestions = new ArrayList<>(questions);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        QuestionsListItemViewMvc viewMvc = new QuestionsListItemViewMvcImpl(mInflater, parent);
+        viewMvc.registerListener(this);
+        return new MyViewHolder(viewMvc);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.mViewMvc.bindQuestion(mQuestions.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mQuestions.size();
+    }
+
+    @Override
+    public void onQuestionClicked(Question question) {
+        mListener.onQuestionClicked(question);
+    }
+
+}
