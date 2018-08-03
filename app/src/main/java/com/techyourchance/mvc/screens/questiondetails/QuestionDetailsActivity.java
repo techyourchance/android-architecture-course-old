c384cba86cb26afcac12b44ad7e92ec1255720ec
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2018-08-03 18:58:34 +0300

    Implementing QuestionDetails screen

--- app/src/main/java/com/techyourchance/mvc/screens/questiondetails/QuestionDetailsActivity.java
+++ app/src/main/java/com/techyourchance/mvc/screens/questiondetails/QuestionDetailsActivity.java
@@ -2,8 +2,21 @@ package com.techyourchance.mvc.screens.questiondetails;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
 
+import com.techyourchance.mvc.R;
+import com.techyourchance.mvc.networking.QuestionDetailsResponseSchema;
+import com.techyourchance.mvc.networking.QuestionSchema;
+import com.techyourchance.mvc.networking.StackoverflowApi;
+import com.techyourchance.mvc.questions.QuestionDetails;
 import com.techyourchance.mvc.screens.common.BaseActivity;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class QuestionDetailsActivity extends BaseActivity {
 
@@ -14,4 +27,64 @@ public class QuestionDetailsActivity extends BaseActivity {
         intent.putExtra(EXTRA_QUESTION_ID, questionId);
         context.startActivity(intent);
     }
+
+    private StackoverflowApi mStackoverflowApi;
+
+    private QuestionDetailsViewMvc mViewMvc;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mStackoverflowApi = getCompositionRoot().getStackoverflowApi();
+        mViewMvc = getCompositionRoot().getViewMvcFactory().getQuestionDetailsViewMvc(null);
+
+        setContentView(mViewMvc.getRootView());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mViewMvc.showProgressIndication();
+        fetchQuestionDetails();
+    }
+
+    private void fetchQuestionDetails() {
+        mStackoverflowApi.fetchQuestionDetails(getQuestionId())
+                .enqueue(new Callback<QuestionDetailsResponseSchema>() {
+                    @Override
+                    public void onResponse(Call<QuestionDetailsResponseSchema> call, Response<QuestionDetailsResponseSchema> response) {
+                        if (response.isSuccessful()) {
+                            bindQuestionDetails(response.body().getQuestion());
+                        } else {
+                            networkCallFailed();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<QuestionDetailsResponseSchema> call, Throwable t) {
+                        networkCallFailed();
+                    }
+                });
+    }
+
+    private String getQuestionId() {
+        return getIntent().getStringExtra(EXTRA_QUESTION_ID);
+    }
+
+    private void bindQuestionDetails(QuestionSchema questionSchema) {
+        mViewMvc.hideProgressIndication();
+        mViewMvc.bindQuestion(
+                new QuestionDetails(
+                        questionSchema.getId(),
+                        questionSchema.getTitle(),
+                        questionSchema.getBody()
+                )
+        );
+    }
+
+    private void networkCallFailed() {
+        mViewMvc.hideProgressIndication();
+        Toast.makeText(this, R.string.error_network_call_failed, Toast.LENGTH_SHORT).show();
+    }
+
 }
---
+++ app/src/main/java/com/techyourchance/mvc/screens/questiondetails/QuestionDetailsViewMvc.java
@@ -0,0 +1,13 @@
+package com.techyourchance.mvc.screens.questiondetails;
+
+import com.techyourchance.mvc.questions.QuestionDetails;
+import com.techyourchance.mvc.screens.common.ViewMvc;
+
+public interface QuestionDetailsViewMvc extends ViewMvc {
+
+    void bindQuestion(QuestionDetails question);
+
+    void showProgressIndication();
+
+    void hideProgressIndication();
+}
