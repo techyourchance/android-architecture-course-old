3286452097dac80f12a3c241a2233c0903b3f884
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2018-08-04 11:26:48 +0300

    Extracting FetchQuestionDetailsUseCase

--- app/src/main/java/com/techyourchance/mvc/screens/questiondetails/QuestionDetailsActivity.java
+++ app/src/main/java/com/techyourchance/mvc/screens/questiondetails/QuestionDetailsActivity.java
@@ -3,22 +3,15 @@ package com.techyourchance.mvc.screens.questiondetails;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
 
 import com.techyourchance.mvc.R;
-import com.techyourchance.mvc.networking.QuestionDetailsResponseSchema;
-import com.techyourchance.mvc.networking.QuestionSchema;
-import com.techyourchance.mvc.networking.StackoverflowApi;
+import com.techyourchance.mvc.questions.FetchQuestionDetailsUseCase;
 import com.techyourchance.mvc.questions.QuestionDetails;
 import com.techyourchance.mvc.screens.common.BaseActivity;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-public class QuestionDetailsActivity extends BaseActivity {
+public class QuestionDetailsActivity extends BaseActivity implements FetchQuestionDetailsUseCase.Listener {
 
     public static final String EXTRA_QUESTION_ID = "EXTRA_QUESTION_ID";
 
@@ -28,14 +21,14 @@ public class QuestionDetailsActivity extends BaseActivity {
         context.startActivity(intent);
     }
 
-    private StackoverflowApi mStackoverflowApi;
+    private FetchQuestionDetailsUseCase mFetchQuestionDetailsUseCase;
 
     private QuestionDetailsViewMvc mViewMvc;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mStackoverflowApi = getCompositionRoot().getStackoverflowApi();
+        mFetchQuestionDetailsUseCase = getCompositionRoot().getFetchQuestionDetailsUseCase();
         mViewMvc = getCompositionRoot().getViewMvcFactory().getQuestionDetailsViewMvc(null);
 
         setContentView(mViewMvc.getRootView());
@@ -44,47 +37,31 @@ public class QuestionDetailsActivity extends BaseActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        mFetchQuestionDetailsUseCase.registerListener(this);
+
         mViewMvc.showProgressIndication();
-        fetchQuestionDetails();
+        mFetchQuestionDetailsUseCase.fetchQuestionDetailsAndNotify(getQuestionId());
     }
 
-    private void fetchQuestionDetails() {
-        mStackoverflowApi.fetchQuestionDetails(getQuestionId())
-                .enqueue(new Callback<QuestionDetailsResponseSchema>() {
-                    @Override
-                    public void onResponse(Call<QuestionDetailsResponseSchema> call, Response<QuestionDetailsResponseSchema> response) {
-                        if (response.isSuccessful()) {
-                            bindQuestionDetails(response.body().getQuestion());
-                        } else {
-                            networkCallFailed();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<QuestionDetailsResponseSchema> call, Throwable t) {
-                        networkCallFailed();
-                    }
-                });
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mFetchQuestionDetailsUseCase.unregisterListener(this);
     }
 
     private String getQuestionId() {
         return getIntent().getStringExtra(EXTRA_QUESTION_ID);
     }
 
-    private void bindQuestionDetails(QuestionSchema questionSchema) {
+    @Override
+    public void onQuestionDetailsFetched(QuestionDetails questionDetails) {
         mViewMvc.hideProgressIndication();
-        mViewMvc.bindQuestion(
-                new QuestionDetails(
-                        questionSchema.getId(),
-                        questionSchema.getTitle(),
-                        questionSchema.getBody()
-                )
-        );
+        mViewMvc.bindQuestion(questionDetails);
     }
 
-    private void networkCallFailed() {
+    @Override
+    public void onQuestionDetailsFetchFailed() {
         mViewMvc.hideProgressIndication();
         Toast.makeText(this, R.string.error_network_call_failed, Toast.LENGTH_SHORT).show();
     }
-
 }
