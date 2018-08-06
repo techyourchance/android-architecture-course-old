6ab9ee7d698ed31cef1b745f6311d1611754482a
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2018-08-06 13:38:51 +0300

    Addition of back button to the toolbar. Exercise 2

--- app/src/main/java/com/techyourchance/mvc/screens/common/toolbar/ToolbarViewMvc.java
+++ app/src/main/java/com/techyourchance/mvc/screens/common/toolbar/ToolbarViewMvc.java
@@ -2,6 +2,7 @@ package com.techyourchance.mvc.screens.common.toolbar;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.techyourchance.mvc.R;
@@ -10,10 +11,12 @@ import com.techyourchance.mvc.screens.common.views.BaseViewMvc;
 public class ToolbarViewMvc extends BaseViewMvc {
 
     private final TextView mTxtTitle;
+    private final ImageButton mBtnBack;
 
     public ToolbarViewMvc(LayoutInflater inflater, ViewGroup parent) {
         setRootView(inflater.inflate(R.layout.layout_toolbar, parent, false));
         mTxtTitle = findViewById(R.id.txt_toolbar_title);
+        mBtnBack = findViewById(R.id.btn_back);
     }
 
     public void setTitle(String title) {
