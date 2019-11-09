4302e3be2637f8a61cf0f72e950271cfc78dec79
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2019-11-09 14:40:33 +0200

    Adding PromptDialog to Questions List screen

--- app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListController.java
+++ app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListController.java
@@ -2,43 +2,74 @@ package com.techyourchance.mvc.screens.questionslist;
 
 import com.techyourchance.mvc.questions.FetchLastActiveQuestionsUseCase;
 import com.techyourchance.mvc.questions.Question;
+import com.techyourchance.mvc.screens.common.dialogs.DialogsEventBus;
+import com.techyourchance.mvc.screens.common.dialogs.DialogsManager;
+import com.techyourchance.mvc.screens.common.dialogs.promptdialog.PromptDialogEvent;
 import com.techyourchance.mvc.screens.common.screensnavigator.ScreensNavigator;
-import com.techyourchance.mvc.screens.common.toastshelper.ToastsHelper;
 
+import java.io.Serializable;
 import java.util.List;
 
 public class QuestionsListController  implements
-        QuestionsListViewMvc.Listener, FetchLastActiveQuestionsUseCase.Listener {
+        QuestionsListViewMvc.Listener,
+        FetchLastActiveQuestionsUseCase.Listener,
+        DialogsEventBus.Listener {
+
+    private enum ScreenState {
+        IDLE, FETCHING_QUESTIONS, QUESTIONS_LIST_SHOWN, NETWORK_ERROR
+    }
 
     private final FetchLastActiveQuestionsUseCase mFetchLastActiveQuestionsUseCase;
     private final ScreensNavigator mScreensNavigator;
-    private final ToastsHelper mToastsHelper;
+    private final DialogsManager mDialogsManager;
+    private final DialogsEventBus mDialogsEventBus;
 
     private QuestionsListViewMvc mViewMvc;
 
+    private ScreenState mScreenState = ScreenState.IDLE;
+
     public QuestionsListController(FetchLastActiveQuestionsUseCase fetchLastActiveQuestionsUseCase,
                                    ScreensNavigator screensNavigator,
-                                   ToastsHelper toastsHelper) {
+                                   DialogsManager dialogsManager,
+                                   DialogsEventBus dialogsEventBus) {
         mFetchLastActiveQuestionsUseCase = fetchLastActiveQuestionsUseCase;
         mScreensNavigator = screensNavigator;
-        mToastsHelper = toastsHelper;
+        mDialogsManager = dialogsManager;
+        mDialogsEventBus = dialogsEventBus;
     }
 
     public void bindView(QuestionsListViewMvc viewMvc) {
         mViewMvc = viewMvc;
     }
 
+    public SavedState getSavedState() {
+        return new SavedState(mScreenState);
+    }
+
+    public void restoreSavedState(SavedState savedState) {
+        mScreenState = savedState.mScreenState;
+    }
+
     public void onStart() {
         mViewMvc.registerListener(this);
         mFetchLastActiveQuestionsUseCase.registerListener(this);
+        mDialogsEventBus.registerListener(this);
 
-        mViewMvc.showProgressIndication();
-        mFetchLastActiveQuestionsUseCase.fetchLastActiveQuestionsAndNotify();
+        if (mScreenState != ScreenState.NETWORK_ERROR) {
+            fetchQuestionsAndNotify();
+        }
     }
 
     public void onStop() {
         mViewMvc.unregisterListener(this);
         mFetchLastActiveQuestionsUseCase.unregisterListener(this);
+        mDialogsEventBus.unregisterListener(this);
+    }
+
+    private void fetchQuestionsAndNotify() {
+        mScreenState = ScreenState.FETCHING_QUESTIONS;
+        mViewMvc.showProgressIndication();
+        mFetchLastActiveQuestionsUseCase.fetchLastActiveQuestionsAndNotify();
     }
 
     @Override
@@ -46,16 +77,42 @@ public class QuestionsListController  implements
         mScreensNavigator.toQuestionDetails(question.getId());
     }
 
-
     @Override
     public void onLastActiveQuestionsFetched(List<Question> questions) {
+        mScreenState = ScreenState.QUESTIONS_LIST_SHOWN;
         mViewMvc.hideProgressIndication();
         mViewMvc.bindQuestions(questions);
     }
 
     @Override
     public void onLastActiveQuestionsFetchFailed() {
+        mScreenState = ScreenState.NETWORK_ERROR;
         mViewMvc.hideProgressIndication();
-        mToastsHelper.showUseCaseError();
+        mDialogsManager.showUseCaseErrorDialog(null);
     }
+
+    @Override
+    public void onDialogEvent(Object event) {
+        if (event instanceof PromptDialogEvent) {
+            switch (((PromptDialogEvent) event).getClickedButton()) {
+                case POSITIVE:
+                    fetchQuestionsAndNotify();
+                    break;
+                case NEGATIVE:
+                    mScreenState = ScreenState.IDLE;
+                    break;
+            }
+        }
+    }
+
+    public static class SavedState implements Serializable {
+        private final ScreenState mScreenState;
+
+        public SavedState(ScreenState screenState) {
+            mScreenState = screenState;
+        }
+    }
+
+
+
 }
